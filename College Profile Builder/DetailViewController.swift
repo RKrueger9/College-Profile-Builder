79d4f9456bde0fb817fef07864c4f9efad923230
RKrueger <student@rkrueger.local>
2016-01-25 12:33:57 -0600

    created detail view controller

---
+++ College Profile Builder/DetailViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DetailViewController.swift
+//  College Profile Builder
+//
+//  Created by RKrueger on 1/25/16.
+//  Copyright © 2016 RKrueger. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController
+{
+    
+    @IBOutlet weak var collegeTextField: UITextField!
+    @IBOutlet weak var stateTextField: UITextField!
+    @IBOutlet weak var enrollmentTextField: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var college : College!
+    
+    override func viewDidLoad()
+    {
+        super.viewDidLoad()
+        collegeTextField.text = college.name
+        stateTextField.text = college.state
+        enrollmentTextField.text = String(college.population)
+        imageView.image = college.image
+    }
+
+    @IBAction func onTappedSaveButton(sender: AnyObject)
+    {
+        college.name = collegeTextField.text!
+        college.state = stateTextField.text!
+        college.population = Int(enrollmentTextField.text!)!
+    }
+    
+}
